164108135bf9a9e36541cc01f860af02cfad46d0
Harry Yihe Yang <96660799+harryyiheyang@users.noreply.github.com>
2024-12-17 16:08:28 -0500

    Update TR_MRBEE_IMRP.R

--- R/TR_MRBEE_IMRP.R
+++ R/TR_MRBEE_IMRP.R
@@ -86,6 +86,7 @@ Rxysum=Rxyall-biasterm(RxyList=RxyList,setdiff(1:n,indvalid))
 theta.complement=center.classifying(delta,theta.source*delta.naive)
 br.complement=c(br-bX%*%theta.complement)
 XtX=t(bX[indvalid,])%*%bX[indvalid,]
+XtX=t(XtX)/2+XtX/2
 Xty=c(t(bX[indvalid,])%*%br.complement[indvalid])
 yty=sum((br.complement[indvalid])^2)
 fit.susie=susie_suff_stat(XtX=XtX,Xty=Xty,yty=yty,L=L,n=length(indvalid),estimate_prior_method="EM",residual_variance=1,s_init=fit.susie,standardize=F,max_iter=susie.iter,intercept=F)
